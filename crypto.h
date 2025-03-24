1e49b8751b570ca631bbfc2836534ef8b1ced0d3
TudorPopa35 <92685759+TudorPopa35@users.noreply.github.com>
2025-03-24 09:24:55 +0200

    Update crypto.h

--- crypto.h
+++ crypto.h
@@ -29,3 +29,51 @@ int modulo(int k, int n)
 	if (n == 0)
 		return -1;
 }
+
+int cmmdc(int a, int b)
+{
+	if (a == 0 || b == 0)
+		return a + b;
+	int r;
+	while (b)
+	{
+		r = modulo(a, b);
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+int invers(int a, int n)
+{
+	int q, r, x0 = 1, x1 = 0, copy_n = n;
+	a = modulo(a, n);
+	while (n != 0)
+	{
+		r = n;
+		q = a / n;
+		n = a % n;
+		a = r;
+
+		r = x1;
+		x1 = x0 - q * x1;
+		x0 = r;
+	}
+	if (a == 1)
+		return modulo(x0, copy_n);
+	return -1;
+}
+
+bool prim(int n)
+{
+	if (n == 0 || n == 1)
+		return 0;
+	if (n == 2)
+		return 1;
+	if (n % 2 == 0)
+		return 0;
+	for (int i = 3; i <= sqrt(n); i += 2)
+		if (n % i == 0)
+			return 0;
+	return 1;
+}
